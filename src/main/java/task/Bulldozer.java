be6b6ef2609fa097c8a4f05c93e30188221e4dbe
Polina <klimashevscapo@gmail.com>
2025-12-02 13:48:28 +0200

    changed all suggestions v2

--- src/main/java/task/Bulldozer.java
+++ src/main/java/task/Bulldozer.java
@@ -4,12 +4,12 @@ public class Bulldozer extends Machine {
 
     @Override
     public void doWork() {
-        System.out.println("Bulldozer is working");
+        System.out.println("Bulldozer started its work");
     }
 
     @Override
     public void stopWork() {
-        System.out.println("Bulldozer is not working");
+        System.out.println("Bulldozer stopped working");
     }
 
 }
--- src/main/java/task/Truck.java
+++ src/main/java/task/Truck.java
@@ -4,12 +4,12 @@ public class Truck extends Machine {
 
     @Override
     public void doWork() {
-        System.out.println("Truck is working");
+        System.out.println("Truck started its work");
     }
 
     @Override
     public void stopWork() {
-        System.out.println("Truck is not working");
+        System.out.println("Truck stopped working");
     }
 
 }
